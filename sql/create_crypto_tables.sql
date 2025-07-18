6dae6d177c2cedf7240e896f7b14fb3053217f58
Your Namexiaosx-GlintAI <shuxian.xiao.jh@gmail.com>
2025-07-18 15:46:12 -0500

    2nd commit

--- sql/create_crypto_tables.sql
+++ sql/create_crypto_tables.sql
@@ -1,1 +1,11 @@
 # create_crypto_tables.sql
+CREATE TABLE IF NOT EXISTS crypto_history (
+    id TEXT,
+    symbol TEXT,
+    name TEXT,
+    current_price FLOAT,
+    market_cap BIGINT,
+    total_volume BIGINT,
+    date DATE,
+    PRIMARY KEY (id, date)
+);
